3ae9b13a7fc72f97ef84cda5adde367f1688badb
bzpetras <bzpetras@cs.ucalgary.ca>
2017-02-08 17:03:45 -0700

    almost there to PhysX. Need MDd version for debug\libs

--- DLoD/boilerplate/headers/Physics/PhysXMain.h
+++ DLoD/boilerplate/headers/Physics/PhysXMain.h
@@ -1,13 +1,11 @@
 #ifndef PHYSXMAIN_H
 #define PHYSXMAIN_H
-/*
+
 #include "PxPhysicsAPI.h"
-#include "vehicle/PxVehicleSDK.h"
-#include "cooking/PxCooking.h"
 #include "PxPhysics.h"
 #include "PxScene.h"
-#include "foundation/PxAllocatorCallback.h"
-*/
+
+
 class PhysXMain
 {
 public:
